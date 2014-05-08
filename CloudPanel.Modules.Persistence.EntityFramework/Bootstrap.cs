6c0c0cee7c2213f88d223cf4efa7c1fb39335cd5
Kaleb Lape <pingortle@gmail.com>
2014-05-08 15:58:16 -0500

    [broken] Add initial migration config.

--- CloudPanel.Modules.Persistence.EntityFramework/Bootstrap.cs
+++ CloudPanel.Modules.Persistence.EntityFramework/Bootstrap.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CloudPanel.Modules.Persistence.Domain;
+using CloudPanel.Modules.Persistence.EntityFramework.Migrations;
 
 // NB: http://stackoverflow.com/questions/16210771/entity-framework-code-first-without-app-config
 namespace CloudPanel.Modules.Persistence.EntityFramework
@@ -17,12 +18,30 @@ namespace CloudPanel.Modules.Persistence.EntityFramework
     }
 
     // Need to support schema migrations here.
-    internal sealed class CloudPanelContextInitializer : CreateDatabaseIfNotExists<CloudPanelContext>
+    internal sealed class CloudPanelContextInitializer : IDatabaseInitializer<CloudPanelContext>
     {
+        #region IDatabaseInitializer<CloudPanelContext> Members
+
+        public void InitializeDatabase(CloudPanelContext context)
+        {
+            var configuration = new Configuration
+            {
+                TargetDatabase = new DbConnectionInfo(context.Database.Connection.ConnectionString, @"System.Data.SqlClient"),
+            };
+
+            var migrator = new DbMigrator(configuration);
+            migrator.Update();
+        }
+
+        #endregion
     }
 
     public sealed class CloudPanelContext : DbContext
     {
+        public CloudPanelContext()
+        {
+        }
+
         public CloudPanelContext(string connectionString)
             : base(connectionString)
         {
---
+++ CloudPanel.Modules.Persistence.EntityFramework/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace CloudPanel.Modules.Persistence.EntityFramework.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CloudPanel.Modules.Persistence.EntityFramework.CloudPanelContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            MigrationsDirectory = @"Migrations";
+        }
+
+        protected override void Seed(CloudPanel.Modules.Persistence.EntityFramework.CloudPanelContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
