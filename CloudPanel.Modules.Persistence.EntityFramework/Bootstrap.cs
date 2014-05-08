ec366d0cbf47acd436e53667e823e7be08e9ec74
Kaleb Lape <pingortle@gmail.com>
2014-05-08 16:03:56 -0500

    [broken] Add entities to context.

--- CloudPanel.Modules.Persistence.EntityFramework/Bootstrap.cs
+++ CloudPanel.Modules.Persistence.EntityFramework/Bootstrap.cs
@@ -53,6 +53,34 @@ namespace CloudPanel.Modules.Persistence.EntityFramework
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
         }
 
+        public DbSet<ApiAccess> ApiAccesses { get; set; }
+        public DbSet<Audit> Audits { get; set; }
+        public DbSet<AuditLogin> AuditLogins { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<CompanyStat> CompanyStats { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<DistributionGroup> DistributionGroups { get; set; }
+        public DbSet<Domain> Domains { get; set; }
+        public DbSet<LogTable> LogTables { get; set; }
+        public DbSet<Plans_Citrix> Plans_Citrix { get; set; }
+        public DbSet<Plans_ExchangeActiveSync> Plans_ExchangeActiveSync { get; set; }
+        public DbSet<Plans_ExchangeMailbox> Plans_ExchangeMailbox { get; set; }
+        public DbSet<Plans_Organization> Plans_Organization { get; set; }
+        public DbSet<Plans_TerminalServices> Plans_TerminalServices { get; set; }
+        public DbSet<PriceOverride> PriceOverrides { get; set; }
+        public DbSet<Price> Prices { get; set; }
+        public DbSet<ResourceMailbox> ResourceMailboxes { get; set; }
+        public DbSet<Setting> Settings { get; set; }
+        public DbSet<Stats_CitrixCount> Stats_CitrixCount { get; set; }
+        public DbSet<Stats_ExchCount> Stats_ExchCount { get; set; }
+        public DbSet<Stats_UserCount> Stats_UserCount { get; set; }
+        public DbSet<SvcMailboxDatabaseSize> SvcMailboxDatabaseSizes { get; set; }
+        public DbSet<SvcMailboxSize> SvcMailboxSizes { get; set; }
+        public DbSet<SvcQueue> SvcQueues { get; set; }
+        public DbSet<SvcTask> SvcTasks { get; set; }
+        public DbSet<UserPermission> UserPermissions { get; set; }
+        public DbSet<UserPlan> UserPlans { get; set; }
+        public DbSet<UserPlansCitrix> UserPlansCitrices { get; set; }
         public DbSet<User> Users { get; set; }
     }
 }
